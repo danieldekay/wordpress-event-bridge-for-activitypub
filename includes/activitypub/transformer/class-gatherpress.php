d89a1cddc2c12fcfc88bb64634d95cf05f9995b9
André Menrath <andre.menrath@posteo.de>
2024-03-07 15:48:42 +0100

    boilerplate gatherpress

---
+++ includes/activitypub/transformer/class-gatherpress.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * ActivityPub GatherPress Event Transformer
+ *
+ * @package activity-event-transformers
+ * @license AGPL-3.0-or-later
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+use Activitypub\Transformer\Post;
+use Activitypub\Activity\Extended_Object\Event;
+use Activitypub\Activity\Extended_Object\Place;
+
+/**
+ * ActivityPub GatherPress Event Transformer
+ *
+ * @since 1.0.0
+ */
+class Gatherpress extends Post {
+	
+}
