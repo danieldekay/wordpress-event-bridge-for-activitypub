93564769018c0bdc629f1df2a9ce48ac2cd813b3
André Menrath <andre.menrath@posteo.de>
2024-11-28 22:05:15 +0100

    fix test initialization

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -74,7 +74,7 @@ function _manually_load_plugin() {
 		case 'eventprime':
 			$plugin_file = 'eventprime-event-calendar-management/event-prime.php';
 			break;
-		case 'event-organiser':
+		case 'event_organiser':
 			$plugin_file = 'event-organiser/event-organiser.php';
 			break;
 	}
