de5a8577968c0d441bd87fa6638a6464f1480b5b
André Menrath <andre.menrath@posteo.de>
2025-04-10 15:20:43 +0200

    ci: update WordPress plugins

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -254,7 +254,7 @@ install_activitypub_plugin() {
 		return;
 	fi
 
-	PLUGIN_VERSION="5.1.0"
+	PLUGIN_VERSION="5.6.1"
 
     URL="https://github.com/Automattic/wordpress-activitypub"
 
@@ -270,15 +270,15 @@ install_wp_plugins() {
 	# Install the one and only ActivityPub plugin (greetings @pfefferle).
 	install_activitypub_plugin
 	# Install (not-activate) all supported event plugins.
-	install_wp_plugin the-events-calendar "6.10.0"
-	install_wp_plugin very-simple-event-list
+	install_wp_plugin the-events-calendar "6.11.2"
+	install_wp_plugin very-simple-event-list "18.1"
 	install_wp_plugin gatherpress
 	install_wp_plugin eventprime-event-calendar-management
-	install_wp_plugin events-manager "6.6.3"
-	install_wp_plugin wp-event-manager "3.1.45.1"
-	install_wp_plugin wp-event-solution "4.0.20"
+	install_wp_plugin events-manager "6.6.4.4"
+	install_wp_plugin wp-event-manager "3.1.48"
+	install_wp_plugin wp-event-solution "4.0.26"
 	install_wp_plugin event-organiser "3.12.8"
-	install_wp_plugin eventon-lite "2.3.2"
+	install_wp_plugin eventon-lite "2.4"
 	# Mec is not installable via wordpress.org, we use our own mirror.
 	install_wp_plugin_mec
 }
