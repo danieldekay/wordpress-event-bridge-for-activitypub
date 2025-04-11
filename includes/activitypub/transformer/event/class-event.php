bba561fe2529366de6633c2292a8b302fe49d9ae
André Menrath <andre.menrath@posteo.de>
2025-04-11 16:43:08 +0200

    remove comments

--- includes/activitypub/transformer/event/class-event.php
+++ includes/activitypub/transformer/event/class-event.php
@@ -571,8 +571,6 @@ abstract class Event extends Post {
 		$activitypub_object = new Event_Object();
 		$activitypub_object = $this->transform_object_properties( $activitypub_object );
 
-		// Manually set boolean  because of https://github.com/Automattic/wordpress-activitypub/issues/1565.
-		// @phpstan-ignore-next-line
 		$activitypub_object->set_comments_enabled( $this->get_comments_enabled() );
 
 		if ( \is_wp_error( $activitypub_object ) ) {
